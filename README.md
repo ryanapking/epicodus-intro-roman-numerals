9a915cb4e71bfbc1061b9710f70e2d8686aecbed
Brian Dutz and Ryan Apking <ryan@>
2016-08-18 12:00:00 -0700

    add so many functions

---
+++ README.md
@@ -0,0 +1,62 @@
+# _Roman Numeral Conversion_
+
+#### _Converts numbers into Roman numerals, 8-18-16_
+
+#### By _**Brian Dutz and Ryan Apking**_
+
+## Description
+
+_Takes user input and translates each word into pig latin._
+
+_Specifications:_
+
+_Output won't accept greater than 3,999_
+* _4000_
+* _invalid input_
+
+_If input is equivalent to a specific symbols value, we return that symbol_
+* _5_
+* _V_
+
+_If input is equivalent to a specific combination of symbol value, we return that symbol combination_
+* _3_
+* _III_
+
+_If input is equivalent to a specific combination of symbol value, we return that symbol combination_
+* _3_
+* _III_
+
+_If a symbol combination would include more than 3 consecutive similar symbols instead subtract from the next highest symbol_
+* _4_
+* _IV_
+
+_Symbols places cannot be skipped_
+* _999_
+* _CMXCIX_
+
+
+
+
+
+## Setup/Installation Requirements
+
+* _Clone from github_
+* _Open HTML file_
+
+## Known Bugs
+
+_None_
+
+## Support and contact details
+
+_Email us with questions_
+
+## Technologies Used
+
+_HTML, CSS, JavaScript, Bootstrap, jQuery_
+
+### License
+
+*This program is licensed under the MIT license.*
+
+Copyright (c) 2016 **_Brian Dutz and Stephen Newkirk_**
---
+++ js/script.js
@@ -0,0 +1,72 @@
+function expressionBuilder(inputNumber) {
+  var values = [1000, 500, 100, 50, 10, 5, 1];
+  var symbols = ["M", "D", "C", "L", "Z", "V", "I"];
+  var romanArray = [];
+  values.forEach(function(value, index) {
+    var remainder = inputNumber % value;
+    var amount = (inputNumber - remainder) / value + symbols[index];
+    romanArray.push(makeSubtraction(amount, index, values, symbols));
+    // romanArray.push(((inputNumber - remainder) / value) + symbols[index]) ;
+    inputNumber = remainder;
+  })
+  var finalArray = fixFives(romanArray);
+  return romanArray;
+}
+
+function makeSubtraction(numberString, index, values, symbols) {
+  if (parseInt(numberString) <= 3) {
+    return numberString;
+  }
+  var number = parseInt(numberString) * values[index];
+  var newString;
+  newString = 1 + symbols[index-1];
+  var difference = values[index-1] - number;
+  newString = buildSubtractionString(difference) + ";" + newString;
+  return newString
+};
+
+function buildSubtractionString(value) {
+  if (value <= 1) {
+    return "1I";
+  } else if (value <= 2) {
+    return "2I";
+  } else if (value <= 10) {
+    return "1Z";
+  } else if (value <= 20) {
+    return "2Z";
+  } else if (value <= 100) {
+    return "1C";
+  } else if (value <= 200) {
+    return "2C";
+  }
+};
+
+function fixFives(romanArray) {
+  var fivehundreds= parseInt(romanArray[1]);
+  var fifties = parseInt(romanArray[3]);
+  var fives = parseInt(romanArray[5]);
+  if (fifties === 0 && fives === 0 && fivehundreds === 0) {
+    return romanArray;
+  }
+  if (fivehundreds > 0 && romanArray[2].includes("D")){
+    romanArray[1] = "0D";
+    romanArray[2] = romanArray[2].replace("D", "M")
+  }
+  if (fifties > 0 && romanArray[4].includes("L")) {
+    romanArray[3] = "0L";
+    romanArray[4] = romanArray[4].replace("L", "C");
+  }
+  if (fives > 0 && romanArray[6].includes("V")) {
+    romanArray[5] = "0V";
+    romanArray[6] = romanArray[6].replace("V", "Z");
+  }
+  return romanArray;
+};
+
+function buildNumeral(inputArray) {
+  var romanString = inputArray.join(";");
+  var romanArray = romanString.split(";");
+  romanArray.forEach(function() {
+// add lots of stuff
+  });
+}
